8d8f0867cbeede2d173b2248a60bf0b017c0cd6d
mosesbabu <mosesarara@gmail.com>
2025-01-19 16:08:40 +0300

    update hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,14 +20,11 @@ const Homepage = () => {
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
-            Crafting Digital Experiences, Designing Tomorrow.
+          Shaping the Future, One Line of Code at a Time!
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-            Welcome to my digital canvas, where innovation and creativity
-            converge. With a keen eye for aesthetics and a mastery of code, my
-            portfolio showcases a diverse collection of projects that reflect my
-            commitment to excellence.
+          Welcome to my digital realm, where creativity meets functionality. As a full-stack developer, I design and develop impactful solutions that blend innovation with seamless user experiences. Explore my portfolio to see the projects that define my journey in building tomorrow’s technology.
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
